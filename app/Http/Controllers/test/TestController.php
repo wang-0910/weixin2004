c5573b4edb34d11fd91f503a9692b53eb8517ad0
wanglele <2482825648@qq.com>
2020-11-05 11:22:00 +0800

    测试

---
+++ app/Http/Controllers/test/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\test;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function test(){
+        echo "小乐乐最好看";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test','test\TestConroller@test');//测试当前方法
